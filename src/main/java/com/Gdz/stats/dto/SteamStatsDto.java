f7ae5c3db76aa94dc78b65c295ee13723c0c74b3
jassonix <jossanix@gmail.com>
2026-03-04 00:41:00 +0300

    Update SteamStatsDto.java

--- src/main/java/com/Gdz/stats/dto/SteamStatsDto.java
+++ src/main/java/com/Gdz/stats/dto/SteamStatsDto.java
@@ -1,13 +1,18 @@
-package com.Gdz.stats.dto;
+package com.Gdz.bot.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class SteamStatsDto {
     private String nickname;
     private int gamesCount;
     private int hoursTotal;
     private String topGame;
+    private String deepSeekAnalysis; // Поле для ответа от ИИ
 }
